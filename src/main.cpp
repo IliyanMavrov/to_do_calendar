496cb590a983f4aa3ff5d09e85236ecaf42c1389
Iliyan Mavrov <ilian_mavrov@abv.bg>
2026-03-27 20:50:00 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
-#include <string>
-
-class Task {
-public:
-    std::string title;
-    bool completed;
-
-    Task(std::string t) : title(t), completed(false) {}
-};
-
 #include <iostream>
+#include <string>
+#include "TaskManager.h"
 
 int main() {
-    std::cout << "Works!\n";
+    TaskManager manager;
+    int choice;
+    std::string title;
+
+    while (true) {
+        std::cout << "\n--- Task Manager ---\n";
+        std::cout << "1. Add task\n";
+        std::cout << "2. Show tasks\n";
+        std::cout << "3. Exit\n";
+        std::cout << "Choice: ";
+
+        std::cin >> choice;
+        std::cin.ignore(); // важно за getline
+
+        if (choice == 1) {
+            std::cout << "Enter task title: ";
+            std::getline(std::cin, title);
+            manager.addTask(title);
+        }
+        else if (choice == 2) {
+            manager.showTasks();
+        }
+        else if (choice == 3) {
+            break;
+        }
+        else {
+            std::cout << "Invalid choice!\n";
+        }
+    }
+
     return 0;
 }
